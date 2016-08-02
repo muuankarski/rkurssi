89f549ca4e030f00c0385c9579264266e7cd2e2c
Markus Kainu <markuskainu@gmail.com>
2016-08-02 08:59:35 +0300

    viesti

--- run.R
+++ run.R
@@ -1,3 +1,10 @@
 rmarkdown::render_site()
 
-# commit
+# commit to gh-pages
+git add .
+git commit -m "viesti"
+git push origin master
+git subtree push --prefix _site/ origin gh-pages
+
+# push to muuankarski.org
+rsync -arv /home/aurelius/btsync/kela/rkurssi/_site/ vadelma_web@muuankarski.org:var/www/html/rkurssi/
